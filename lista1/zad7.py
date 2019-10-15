af95e93ce533f2d3d04fbbb39c195a88af2bfa97
fporucznik <porucznik199@wp.pl>
2019-10-15 23:59:15 +0200

    zad7 final

---
+++ lista1/zad7.py
@@ -0,0 +1,2 @@
+#znak _ sluzy do przywolania poprzedniego wyniku obliczen
+#np. wpisujac w trybie interaktywnym 3+3 wynikiem bedzie 6, nastepnie wpisujac _ wyswietli nam 6, wpisujac _+3 wyswietli nam 9
