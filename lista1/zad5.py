209b42e28b9b1e631da3e8c90bee3a6902920132
fporucznik <porucznik199@wp.pl>
2019-10-15 23:27:44 +0200

    zad5 final

---
+++ lista1/zad5.py
@@ -0,0 +1,5 @@
+import math
+print(5//2)
+print(round(5.2345, 2)) #zaokraglenie do 2 miejsc po przecinku
+print(round((5.5345))) #zaokraglenie do liczby calkowitej
+print(math.floor(5.2345)) #zwraca najwieksza liczbe calkowita nie wieksza od podanej liczby
