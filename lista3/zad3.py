879199eea3c3d02443078f64c9eed2784be322f2
FPorucznik <porucznik199@wp.pl>
2019-10-29 17:36:51 +0100

    zad3 final

--- lista3/zad3.py
+++ lista3/zad3.py
@@ -1,9 +1,26 @@
+import math
 a=int(input())
 b=int(input())
 c=int(input())
-if a==0:
-    print("To nie jest równanie kwadratowe!")
-else:
-    def kwadratowa(a,b,c):
+def kwadratowa(a,b,c):
+
+    if a==0:
+        print("To nie jest równanie kwadratowe!")
+    else:
+        delta=b**2-(4*a*c)
+        if(delta>0):
+            x1=(-b-math.sqrt(delta))/(2*a)
+            x2=(-b+math.sqrt(delta))/(2*a)
+            wynik=(x1,x2)
+            print(wynik)
+        elif(delta==0):
+            x2="brak"
+            x1=(-b)/(2*a)
+            wynik=(x1,x2)
+            print(wynik)
+        else:
+            print("brak pierwiastkow")
+
+kwadratowa(a,b,c)
         
 
