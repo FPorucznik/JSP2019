e073f3b67600a3b65079b26bf85bd9b15fb938e8
FPorucznik <porucznik199@wp.pl>
2019-11-25 18:50:42 +0100

    zad1 progress

--- lista6/trojkat.py
+++ lista6/trojkat.py
@@ -1,2 +1,18 @@
+import math
 def obwod(a,b,c):
-    return 
+    return a+b+c
+
+def pole(a,b,c):
+    polObwodu=(a+b+c)/2
+    p=math.sqrt(polObwodu*(polObwodu-a)*(polObwodu-b)*(polObwodu-c))
+    return p
+
+def jakiBoczny(a,b,c):
+    if a==b==c:
+        return 'Trójkąt równoboczny'
+    elif (a==b and a!=c) or (a==c and a!=b) or (b==c and b!=a):
+        return 'Trójkąt równoramienny'
+    else:
+        return 'Trójąt różnoboczny'
+
+jakiBoczny(1,1,2)
