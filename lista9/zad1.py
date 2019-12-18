6d357226c0b1d22c3e285d03d7ababe5bd541380
FPorucznik <porucznik199@wp.pl>
2019-12-18 12:36:44 +0100

    zad1 final

---
+++ lista9/zad1.py
@@ -0,0 +1,7 @@
+import numpy as np
+A=np.array([[1,2,3,-2,-1],[3,5,5,-3,-9],[2,3,2,0,-8],[2,6,7,-5,1],[1,2,6,-4,-10]])
+B=np.array([6,2,-5,17,12])
+X=np.linalg.solve(A,B)
+czyDziala=np.allclose(np.dot(A,X),B)
+print(X)
+print(czyDziala)
