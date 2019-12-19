d8868bf8982c23f74b6fdbbabb1bdd06439f64c8
FPorucznik <porucznik199@wp.pl>
2019-12-19 02:52:11 +0100

    zad2 final

--- lista9/statystyka.py
+++ lista9/statystyka.py
@@ -1,5 +1,27 @@
 import sys
-print(sys.argv[1])
-print(sys.argv[2])
-print(sys.argv[3])
-print(sys.argv[4])
+import numpy as np
+def zArg():
+    argumenty=sys.argv[1:]
+    argumenty=[int(x) for x in argumenty]
+
+def zPliku(arg):
+    b = list()
+    with open(arg) as filename:
+        for line in filename:
+            line = line.replace("\r\n", "")
+            b.append(int(line))
+    return b
+
+def statystyki(arg):
+    print(np.average(arg))
+    print(np.var(arg))
+    print(np.std(arg))
+
+if ".txt" in sys.argv[1]:
+    a=zPliku(sys.argv[1])
+    for i in a:
+        statystyki(i)
+else:
+    zArg()
+    for i in argumenty:
+        statystyki(i)
