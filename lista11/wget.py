76bceb9d9ef4c9134c204ca5339742368b3f6ab7
FPorucznik <porucznik199@wp.pl>
2020-01-23 00:53:31 +0100

    wget final

--- lista11/wget.py
+++ lista11/wget.py
@@ -2,15 +2,16 @@ import urllib.request
 from urllib.parse import urlparse
 l=input("Podaj URL: ")
 def zapis(link):
-    try:
-        with urllib.request.urlopen(link) as url:
-            parsed=urlparse(link)
-            s=url.read()
-            return parse
-            #sOut = open()
-    except urllib.error.HTTPError:
-        return "Strona nie została znaleziona"
-    except urllib.error.URLError:
-        return "Strona nie została znaleziona"
-
-zapis(url)
+    o=urlparse(link)
+    path=o.path
+    name=[]
+    if path.endswith("/"):
+        name='index.html'
+    else:
+        length=len(path)-1
+        while path[length] != '/':
+            name.append(path[length]) 
+            length-=1
+        name=''.join(list(reversed(name)))+'.html'
+    urllib.request.urlretrieve(link, name)
+print(zapis(l))
